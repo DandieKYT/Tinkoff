5a211e8cc48bcccde2c15481236fbeb579feebb2
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-07-15 18:45:50 +0300

    5 push

--- src/test/java/pages/PrivateOfficePage.java
+++ src/test/java/pages/PrivateOfficePage.java
@@ -1,48 +1,95 @@
 package pages;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.ui.Select;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 import static io.qameta.allure.Allure.step;
 
 public class PrivateOfficePage {
+    private SelenideElement
+            privateOfficeHover = $x("//span[text()='Личный кабинет']"),
+            internetBank =$x("//li/div/a/span/div"),
+            bisness =$x("//li[2]/div/a/span/div"),
+            deposite =$x("//li[3]/div/a/span/div"),
+            mobileTinkoff =$x("//li[4]/div/a/span/div"),
+            checkoutInternetBank =$x("//*[@id='form-title']"),
+            checkoutBisness = $x("//*[@id='form-title']"),
+            checkoutDeposite = $x("//*[@id='form-title']"),
+            checkoutMobileTinkoff =$x("//*[@id='form-title']");
 
-    PrivateOfficePageLocator locators = new PrivateOfficePageLocator();
 
-    public PrivateOfficePage openTitle(String param) {
-        step("Открытие вкладки", () -> {
-            locators.openTitle(param).click();
+    public PrivateOfficePage internetBank() {
+        step("Открытие интернет-банка", () -> {
+            privateOfficeHover.hover();
+            internetBank.click();
+            Selenide.switchTo().window(1);
         });
-        return null;
+        return this;
     }
-    public PrivateOfficePage hoverPrivateOffice(String param) {
+    public PrivateOfficePage privateOfficeHover() {
         step("Наведение курсора на личный кабинет", () -> {
-            locators.hoverPrivateOffice.hover();
+            privateOfficeHover.click();
         });
-        return null;
+        return this;
     }
-    public PrivateOfficePage checkTitle(String expectedText) {
-        step("Проверка содержимого вкладки", () -> {
-            locators.checkTitle(expectedText).shouldBe(text(expectedText));
+    public PrivateOfficePage bisness() {
+        step("Открытие Тинькофф-Бизнес", () -> {
+            privateOfficeHover.hover();
+            bisness.click();
+            Selenide.switchTo().window(1);
         });
-        return null;
+        return this;
     }
-
-
-    public class PrivateOfficePageLocator {
-
-        public SelenideElement hoverPrivateOffice = $x("//span[text()='Личный кабинет']");
-
-        public SelenideElement openTitle(String param) {
-            return $x(String.format("//span[text()='%s']", (param)));
-        }
-
-        public SelenideElement checkTitle(String expectedText) {
-            return $((byTagAndText("h1", (expectedText))));
-        }
-
+    public PrivateOfficePage deposite() {
+        step("Открытие Инвестиций", () -> {
+            privateOfficeHover.hover();
+            deposite.click();
+            Selenide.switchTo().window(1);
+        });
+        return this;
+    }
+    public PrivateOfficePage mobileTinkoff() {
+        step("Открытие Тинькофф-Мобайл", () -> {
+            privateOfficeHover.hover();
+            mobileTinkoff.click();
+            Selenide.switchTo().window(1);
+        });
+        return this;
+    }
+    public PrivateOfficePage checkoutInternetBank() {
+        step("Проверка заголовка странцы авторизации Интернет-банка", () -> {
+            checkoutInternetBank.shouldBe(Condition.text("Вход в Тинькофф"));
+            Selenide.closeWindow();
+            Selenide.switchTo().window(0);
+        });
+        return this;
+    }
+    public PrivateOfficePage checkoutBisness() {
+        step("Проверка заголовка странцы авторизации Тинькофф-Бизнес", () -> {
+            checkoutBisness.shouldBe(Condition.text("Вход в Тинькофф Бизнес"));
+            Selenide.closeWindow();
+            Selenide.switchTo().window(0);
+        });
+        return this;
+    }
+    public PrivateOfficePage checkoutDeposite() {
+        step("Проверка заголовка странцы авторизации Инвестиции", () -> {
+            checkoutDeposite.shouldBe(Condition.text("Вход в Тинькофф"));
+            Selenide.closeWindow();
+            Selenide.switchTo().window(0);
+        });
+        return this;
+    }
+    public PrivateOfficePage checkoutMobileTinkoff() {
+        step("Проверка заголовка странцы авторизации Мобайл-Тинькофф", () -> {
+            checkoutMobileTinkoff.shouldBe(Condition.text("Вход в Мобайл"));
+            Selenide.closeWindow();
+            Selenide.switchTo().window(0);
+        });
+        return this;
     }
 }
--- src/test/java/tests/PrivateOfficeTest.java
+++ src/test/java/tests/PrivateOfficeTest.java
@@ -4,25 +4,22 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class PrivateOfficeTest extends TestBase {
-    @Tag("productsAndCompany")
-    @CsvSource(value = {
-            "Интернет-банк ,      Вход в Тинькофф",
-            "Тинькофф Бизнес,     Вход в Тинькофф Бизнес",
-            "Инвестиции,           Вход в Тинькофф",
-            "Тинькофф Мобайл,       Вход в Мобайл"
-    })
-    @Owner("Kudryavtsev")
-    @Feature("Продукты и Компания")
-    @Story("Открытие страниц Продукты и Компания и проверка их содержимого")
-    @ParameterizedTest
-    public void productsAndCompany(String param, String expectedText) {
+
+    @Test
+    public void productsAndCompany() {
         basePage.openPage();
-        privateOfficePage.hoverPrivateOffice(null);
-        privateOfficePage.openTitle(param);
-        privateOfficePage.checkTitle(expectedText);
+        privateOfficePage.internetBank();
+        privateOfficePage.checkoutInternetBank();
+        privateOfficePage.bisness();
+        privateOfficePage.checkoutBisness();
+        privateOfficePage.deposite();
+        privateOfficePage.checkoutDeposite();
+        privateOfficePage.mobileTinkoff();
+        privateOfficePage.checkoutMobileTinkoff();
     }
 }
